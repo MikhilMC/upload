c493b1c65d3e10e54b9e97fd4cb9e5117cc77d71
MikhilMC <mikhilmohan13@gmail.com>
2021-12-06 19:08:10 +0530

    small edit in the code

--- models/User.js
+++ models/User.js
@@ -1,21 +1,37 @@
-const mongoose = require('mongoose');
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+
+const mongoURI = process.env.MONGO_URI;
+
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("start");
+  })
+  .catch((err) => {
+    console.error("App starting error:", err.stack);
+    process.exit(1);
+  });
+
+const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   email: {
     type: String,
-    required: true
+    required: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   profilePictureId: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
-    ref: 'ProfilePicture'
-  }
-})
+    ref: "ProfilePicture",
+  },
+});
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
--- routes/register.js
+++ routes/register.js
@@ -1,45 +1,13 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
-
 const express = require("express");
-const mongoose = require("mongoose");
 
 const upload = require('../helper/media');
 const User = require("../models/User");
 
 const router = express.Router();
 
-const mongoURI = process.env.MONGO_URI;
-
-const client = mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('start');
-})
-.catch(err => {
-  console.error('App starting error:', err.stack);
-  process.exit(1)
-});
-
-// client.connect()
-//   .then(() => {
-//      console.log('start');
-//   })
-//   .catch(err => {
-//      console.error('App starting error:', err.stack);
-//      process.exit(1)
-//   });
-
 router.post("/", upload.single("file"), async (req, res) => {
   // console.log(req.body);
   // console.log(req.file);
-  // res.send({
-  //   body: req.body,
-  //   file: req.file
-  // });
-
   const fileId = req.file != null ? req.file.id : null;
   console.log(fileId);
   const user = new User({
